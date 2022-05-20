8191f7f3843361040a0b75a609124328b57a58e0
Arnaud Drain <drain.arnaud@hotmail.fr>
2022-05-20 11:02:03 +0200

    chore(text): update label

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to my app!</p>
+        <p>Welcome to my app</p>
         <a
           className="App-link"
           href="https://reactjs.org"
