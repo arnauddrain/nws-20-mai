b72dddfec9af289eb918492bc9a66cc0b9ea3328
Arnaud Drain <drain.arnaud@hotmail.fr>
2022-05-20 12:10:02 +0200

    test(cypress): install cypress and first test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
 function App() {
+  const [text, setText] = React.useState("");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to my app</p>
+        <input onChange={(e) => setText(e.target.value)}></input>
+        <p data-cy="upperText">{text.toUpperCase()}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
